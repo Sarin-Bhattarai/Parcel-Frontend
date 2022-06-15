76301ec4b5ced2b460eb1d59ed8a7fd6e523f647
sarin punk <sarin.bhattarai@icp.edu.np>
2022-06-15 17:36:43 +0545

    Read parcel complete

--- src/pages/Parcel.js
+++ src/pages/Parcel.js
@@ -1,47 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { getAllParcels } from "../utils/others/parcelApi";
 import Card from "../components/Card";
+import "../index.css";
 
 const Parcel = () => {
   const [parcels, setParcels] = useState([]);
 
-  const loadParcels = () => {
-    getAllParcels().then((data) => {
-      console.log(data);
-      if (data.error) {
-        console.log(data.error);
-      } else {
-        setParcels(data);
-      }
-    });
-  };
-
   useEffect(() => {
-    loadParcels();
+    getAllParcels().then((data) => setParcels(data.data.parcel));
   }, []);
 
   return (
-    <div>
-      <h3>Total {parcels.length} Parcels</h3>
-      <br />
-      <Card
-        code="Parcel Code"
-        imageUrl="https://images.unsplash.com/photo-1513201099705-a9746e1e201f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHBhcmNlbHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-        body="View for more details about parcel and to update and delete parcel"
-      />
+    <div className="container">
+      {parcels &&
+        parcels.map((p, i) => {
+          return (
+            <Card
+              key={i}
+              code={p.code}
+              imageUrl="https://images.unsplash.com/photo-1513201099705-a9746e1e201f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHBhcmNlbHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+              body="View for more details about parcel and to update and delete parcel"
+            >
+              <p>{p.code}</p>
+            </Card>
+          );
+        })}
     </div>
   );
 };
 
 export default Parcel;
-//  useEffect(() => {
-//    async function getParcels() {
-//      const res = await axios.get(`${API}/parcel`);
-//      console.log(res.data);
-//      setParcels(res.data);
-//    }
-//    getParcels();
-//  }, []);
-
-// import { API } from "../config";
-// import axios from "axios";
